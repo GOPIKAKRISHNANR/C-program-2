6fa4108c756a296ca5932ff75b6fde7ff3fc6612
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-25 20:22:46 +0530

    Update interchange.c

--- interchange.c
+++ interchange.c
@@ -5,11 +5,12 @@ output a=m,b=k
 #include<stdio.h>
 int main()
 {
-  int a,b;
+  int a,b,c;
   printf("Enter the numbers :");
   scanf("%d%d",&a,&b);
-  a=a+b;
-  b=a-b;
-  a=a-b;
-  printf("a=%d\nb=%d\n",a,b);
+  c=a+b;
+  b=c-b;
+  a=c-b;
+  printf("a=%d",a);
+  printf("b=%d",b);
 }
