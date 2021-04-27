be80b1fbd68b5f48c40c7622f85651b32656a0df
GOPIKAKRISHNANR <82164744+GOPIKAKRISHNANR@users.noreply.github.com>
2021-04-27 20:22:05 +0530

    Update wcf.c

--- wcf.c
+++ wcf.c
@@ -10,6 +10,6 @@ int main()
   scanf("%f",&t);
   printf("Enter the wind velocity :");
   scanf("%f",&v);
-  wcf=35.74 + 0.6215*t + (0.4275*t) * pow(v,0.16);
+  wcf=35.74 + 0.6215*t + ((0.4275*t)-35.75) * pow(v,0.16);
   printf("wind chill factor is %f\n",wcf);
 }
